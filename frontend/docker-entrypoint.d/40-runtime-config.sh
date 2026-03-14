be57ee90b75e5ad018c0462105187ea6e38cc0cb
Othman Salahi <132019758+OthmanSALAHI@users.noreply.github.com>
2026-03-14 06:05:53 +0000

    Update 40-runtime-config.sh

--- frontend/docker-entrypoint.d/40-runtime-config.sh
+++ frontend/docker-entrypoint.d/40-runtime-config.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 set -eu
-
 cat <<EOF >/usr/share/nginx/html/config.js
 window.__APP_CONFIG__ = {
-  VITE_API_BASE_URL: "${VITE_API_BASE_URL:-http://localhost:8000}"
+  VITE_API_BASE_URL: "${VITE_API_BASE_URL:-}"
 };
 EOF
